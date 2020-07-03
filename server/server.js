ec99d7f2c5366a62a4da8aa459171b6425bec49c
Евгений Рипачев <e.ripachev@gmail.com>
2020-07-03 22:56:05 +0300

    -

--- server/server.js
+++ server/server.js
@@ -21,7 +21,20 @@ client.connect();
 
 
 app.get('/classes', async (req, res) => {
-    const query = `SELECT * FROM class`;
+    const version_number = req.query.version_number
+    let query = ""
+    if (version_number){
+        query = `SELECT cl.* FROM "class" as cl
+        left join vocabulary_metrics as voc
+        on voc.id = cl.vocabulary_id
+        where voc.name in (
+            SELECT name FROM vocabulary_metrics
+            GROUP BY name
+            HAVING count(*)>=${version_number}
+        )`;
+    } else {
+        query = `SELECT * FROM class`;
+    }
     client.query(query, (err, res1) => {
         if (err) {
             console.error(err);
@@ -33,7 +46,18 @@ app.get('/classes', async (req, res) => {
 
 
 app.get('/vocabularies', async (req, res) => {
-    const query = `SELECT * FROM vocabulary_metrics`;
+    const version_number = req.query.version_number
+    let query = ""
+    if (version_number){
+        query = `SELECT * FROM vocabulary_metrics
+        where name in (
+            SELECT name FROM vocabulary_metrics
+            GROUP BY name
+            HAVING count(*)>=${version_number}
+        )`;
+    } else {
+        query = `SELECT * FROM vocabulary_metrics`;
+    }
     client.query(query, (err, res1) => {
         if (err) {
             console.error(err);
