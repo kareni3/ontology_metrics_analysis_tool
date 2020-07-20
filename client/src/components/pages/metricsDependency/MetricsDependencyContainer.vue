40bcb4d23cad7fb3e6740422332acaeb3b3ebf6e
Евгений Рипачев <e.ripachev@gmail.com>
2020-07-20 19:55:29 +0300

    add pearson coefficient

--- client/src/components/pages/metricsDependency/MetricsDependencyContainer.vue
+++ client/src/components/pages/metricsDependency/MetricsDependencyContainer.vue
@@ -34,7 +34,33 @@
       <input class="checkbox" type="checkbox" v-model="isOnlyLastVersion" /> Only Last Version
     </div>
     <div>
+      Pearson correlation coefficient:
+      <strong v-if="r">{{r.toFixed(3)}}</strong>
+      <span class="pointer btn" @click="openDialog">Open List of all coefficients</span>
+    </div>
+    <div>
       <ScatterPoints v-if="Object.values(vocabularyList).length" :data="vocabularyList" />
+    </div>
+    <div v-show="dialog" class="dialog">
+      <div>
+        <span @click="closeDialog" class="close">x</span>
+        <v-select
+          class="selector mr-12 mb-12"
+          v-if="R.length !== 0"
+          placeholder="Choose Metric"
+          v-model="selectedMetricForPearson"
+          :options="$store.state.vocabularyMetricNames"
+        ></v-select>
+        <div class="item" v-for="(r) in computedR" :key="r.m1+r.m2">
+          <span class="left">{{r.m1}} / {{r.m2}}:</span>
+          <strong
+            class="right"
+            :style="'color:rgb('+(180-Math.ceil(Math.abs(r.r)*255))+','+(Math.ceil(Math.abs(r.r)*255*0.6))+',0)'"
+          >{{r.r.toFixed(3)}}</strong>
+        </div>
+        <div class="stand_by" v-if="R.length === 0">Please, stand by... I am thinking</div>
+        <div class="pointer show_more" v-else @click="maxInd+=50">Show More</div>
+      </div>
     </div>
   </div>
 </template>
@@ -55,10 +81,14 @@ export default {
       enabledVocabularyNames: [],
       selectedMetricX: "",
       selectedMetricY: "",
+      selectedMetricForPearson: "",
+      dialog: false,
+      maxInd: 50,
       metricX: {
         min: 0,
         max: 100000
       },
+      R: [],
       metricY: {
         min: 0,
         max: 100000
@@ -75,10 +105,25 @@ export default {
     };
   },
   watch: {
+    selectedMetricForPearson() {
+      this.R = [];
+      setTimeout(() => {
+        this.calcR();
+      }, 200);
+    },
+    dialog(v) {
+      if (v && this.R.length === 0) {
+        this.enabledVocabularyNamesUpdated = false;
+        setTimeout(() => {
+          this.calcR();
+        }, 200);
+      }
+    },
     enabledVocabularyNames() {
       if (this.selectedMetricY && this.selectedMetricX) {
         this.prepareData();
       }
+      this.R = [];
     },
     selectedMetricX(v) {
       if (this.selectedMetricY) {
@@ -98,6 +143,34 @@ export default {
       }
     }
   },
+  computed: {
+    computedR() {
+      return this.R.filter((el, ind) => ind < this.maxInd);
+    },
+    r() {
+      if (!this.selectedMetricX || !this.selectedMetricY) return null;
+      const avg = [0, 0];
+      let n = 0;
+      Object.values(this.vocabularyList).forEach(el => {
+        avg[0] += +el[0].x;
+        avg[1] += +el[0].y;
+        n++;
+      });
+      avg[0] /= n;
+      avg[1] /= n;
+      let numerator = 0;
+      let denominator = 0;
+      let denominatorX = 0;
+      let denominatorY = 0;
+      Object.values(this.vocabularyList).forEach(el => {
+        numerator += (+el[0].x - avg[0]) * (+el[0].y - avg[1]);
+        denominatorX += (+el[0].x - avg[0]) ** 2;
+        denominatorY += (+el[0].y - avg[1]) ** 2;
+      });
+      denominator = Math.sqrt(denominatorX * denominatorY);
+      return numerator / denominator;
+    }
+  },
   mounted() {
     this.$store.dispatch("fetchGraphCount", 5);
     this.setCheckboxNames();
@@ -107,10 +180,68 @@ export default {
     }
   },
   methods: {
+    closeDialog() {
+      this.dialog = false;
+    },
+    openDialog() {
+      this.dialog = true;
+    },
     confirmMinMax() {
       this.confirmedMetricX = this.metricX;
       this.confirmedMetricY = this.metricY;
       this.prepareData();
+    },
+    calcR() {
+      this.R = [];
+      const vList = new Object();
+      this.$store.state.vocabularies.forEach(vocabulary => {
+        if (!this.enabledVocabularyNames.includes(vocabulary.name)) return;
+        Object.entries(vocabulary).forEach(v1 => {
+          Object.entries(vocabulary).forEach(v2 => {
+            if (v1[0] === v2[0]) return;
+            if (this.selectedMetricForPearson && v1[0] !== this.selectedMetricForPearson) return;
+            if (isNaN(+v1[1])) return;
+            if (isNaN(+v2[1])) return;
+            const name = this.isOnlyLastVersion
+              ? `${vocabulary.name}`
+              : `${vocabulary.name} v${vocabulary.version_name}`;
+            if (!vList[v1[0] + "&&" + v2[0]]) vList[v1[0] + "&&" + v2[0]] = {};
+            vList[v1[0] + "&&" + v2[0]][name] = [];
+            vList[v1[0] + "&&" + v2[0]][name].push({
+              x: v1[1],
+              y: v2[1]
+            });
+          });
+        });
+      });
+      Object.entries(vList).forEach(list => {
+        const avg = [0, 0];
+        let n = 0;
+        Object.values(list[1]).forEach(el => {
+          avg[0] += +el[0].x;
+          avg[1] += +el[0].y;
+          n++;
+        });
+        avg[0] /= n;
+        avg[1] /= n;
+        let numerator = 0;
+        let denominator = 0;
+        let denominatorX = 0;
+        let denominatorY = 0;
+        Object.values(list[1]).forEach(el => {
+          numerator += (+el[0].x - avg[0]) * (+el[0].y - avg[1]);
+          denominatorX += (+el[0].x - avg[0]) ** 2;
+          denominatorY += (+el[0].y - avg[1]) ** 2;
+        });
+        denominator = Math.sqrt(denominatorX * denominatorY);
+        if (isNaN(numerator / denominator)) return;
+        this.R.push({
+          r: numerator / denominator,
+          m1: list[0].slice(0, list[0].indexOf("&&")),
+          m2: list[0].slice(list[0].indexOf("&&") + 2)
+        });
+      });
+      this.R.sort((a, b) => Math.abs(b.r) - Math.abs(a.r));
     },
     prepareData() {
       this.vocabularyList = new Object();
@@ -189,5 +320,40 @@ export default {
   right: 0;
   padding: 6px 24px;
   position: absolute;
+}
+.btn {
+  border: 1px solid #00000040;
+  border-radius: 0.5rem;
+  padding: 2px 4px;
+  background-color: rgba(139, 188, 228, 0.4);
+  margin-left: 12px;
+}
+.dialog {
+  & > div {
+    padding: 48px;
+    text-align: left;
+  }
+  .item {
+    max-width: 900px;
+    margin: auto;
+    padding: 6px 0;
+    height: auto;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #00000020;
+  }
+  .show_more {
+    font-size: 1.3rem;
+    padding: 12px;
+    justify-content: center;
+    text-align: center;
+  }
+  .stand_by {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+  }
 }
 </style>
